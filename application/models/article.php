159d8e23b55e0c359b1160ef11d7e035a1900b19
Satria W Sandi <amartuis@gmail.com>
2015-06-11 19:43:01 +0700

    function article detail dan comment list untuk article tertentu

--- application/models/article.php
+++ application/models/article.php
@@ -7,5 +7,19 @@
 			$query=$this->db->get();
 			return $query->result_array();
 		}
+
+		function get_article_detail($article_ID)
+		{
+			$this->db->select('title, DATE_FORMAT(tbl_cyberits_t_article.created,\'%d %b %y\') AS created ,username ,view ,content ,AVG(rating) as rating_per_article, img_link, user_img')->from('tbl_cyberits_t_article')->join('tbl_cyberits_m_user', 'tbl_cyberits_m_user.user_ID = tbl_cyberits_t_article.author_ID')->join('tbl_cyberits_t_comment','tbl_cyberits_t_comment.article_ID = tbl_cyberits_t_article.article_ID' )->order_by('tbl_cyberits_t_article.created','desc')->group_by('tbl_cyberits_t_article.article_ID');
+			$query=$this->db->get();
+			return $query->result_array();
+		}
+
+		function get_article_comments($num=20, $start=0, $article_ID)
+		{
+			$this->db->select('DATE_FORMAT(tbl_cyberits_t_comment.created,\'%d %b %y\') AS created, name, rating, comment')->from('tbl_cyberits_t_comment')->where('article_ID', $article_ID)->order_by('tbl_cyberits_t_comment.created')->limit($num,$start);
+			$query=$this->db->get();
+			return $query->result_array();
+		}
 	}
 ?>
