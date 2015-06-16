de447ebbabf4084d64b3ae1157859e5ef7f1f1bd
Satria W Sandi <amartuis@gmail.com>
2015-06-16 19:23:49 +0700

    merombak model user sesuai standar koding dan database baru

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,5 +1,14 @@
 <?php  
-	class User extends CI_Model
+    /*
+    ===================================================================================
+    Created By              Satria W Sandi                  on 18.14
+    Model                   User_Model                      for tbl_cyberits_m_users
+    ===================================================================================
+    functions :
+    1. login                Mencocokkan username dan password dengan data user yang ada di database
+    2.
+    */
+	class User_Model extends CI_Model
 	{
 		function login($username,$password)
 		{
@@ -7,11 +16,11 @@
 				'username'=>$username,
 				'password'=>sha1($password)
 			);
-			$this->db->select()->from('tbl_cyberits_m_user')->where($where);
+			$this->db->select()->from('tbl_cyberits_m_users')->where($where);
 			$query=$this->db->get();
 			return $query->first_row('array');
 		}
-
+/*
 		function get_tag_list()
 		{
 			$this->db->select('tag_ID,tag')->from('tbl_cyberits_t_tag');
@@ -31,5 +40,6 @@
 			$this->db->insert('tbl_cyberits_t_article',$data);
 			return $this->db->insert_id();
 		}
+        */
 	}
 ?>
