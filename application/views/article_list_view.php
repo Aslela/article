7a3a9d7b5341877a02affc2e6565e3b1da06392d
Aslela <vickysiswanto@gmail.com>
2015-06-17 12:37:45 +0700

    Article List, plus gambar dan rating star

--- application/views/article_list_view.php
+++ application/views/article_list_view.php
@@ -4,18 +4,45 @@
     $no++;
   ?>
 
+<script>
+    $(function(){  
+        $('input.star').rating(); 
+    });
+</script>
+
  <div class="row content" id="content-<?=$no?>">
-    <h1><?=$row['title']?></h1>					
+    <h2><?=$row['title']?></h2>					
     <div class="row">
 		<div class="col-md-4 col-lg-4">
 		  <h4><?=$row['created']?> by <b><?=$row['username']?></b></h4>
 		</div>
 								
         <div class="col-md-4 col-lg-4 viewer">
-		  <h4><?=$row['view']?></h4>
+		  <h4>View : <?=$row['view']?></h4> 
 		</div>
 							
 	   <div class="col-md-4 col-lg-4 share-bar">
+            <h4>
+                <div id="star-rating">
+                    <?php 
+                        for($i=1;$i<=5;$i++){
+                            if($i==floor($row['ratingPerArticle'])){
+                                echo '<input class="star" name="star1" type="radio" value="'.$i.'" disabled="disabled" checked="checked"/>';
+                               
+                            }else{
+                                echo '<input class="star" name="star1" type="radio" value="'.$i.'" disabled="disabled"/>';
+                            }
+                        }
+                    ?>
+                </div>
+              
+			</h4>
+            <div class="clear"></div>
+	   </div>
+    </div><!--Content Title-->
+    
+    <div class="row">
+         <div class="col-md-4 col-lg-4 share-bar">
             <h4>
                 <ul>
 					<li class="fb">
@@ -33,21 +60,26 @@
 				</ul>
 			</h4>
             <div class="clear"></div>
-		    </div>
-		</div><!--Content Title-->
+	   </div>
+    </div>
 						
-		<div class="row">
+	<div class="row">
+			
+        <div class="col-md-4 col-lg-4 img-content">
+            <img src="<?php echo base_url(); ?>img/<?=$row['categoryImgLink']?>" height="200" width="200">
+        </div>	
+            			
+		<div class="col-md-8 col-lg-8 desc">
+			<p>
+				<?=$row['content']?>...
+			</p>
 							
-			<div class="col-md-12 col-lg-12 desc">
-				<p>
-					<?=$row['content']?>...
-				</p>
-							
-				<div class="btn-group">
-					<a href="#"><button type="button" class="btn btn-info"> Read More..</button></a>
-				</div>
+			<div class="btn-group">
+				<a href="#"><button type="button" class="btn btn-info"> Read More..</button></a>
 			</div>
-		</div><!--Content Short Desc-->
+                
+		</div>
+	</div><!--Content Short Desc-->
 </div><!--Content List-->
 <?php
    }
